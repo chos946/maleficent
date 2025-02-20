14e709e7459ff5e81419fc33e5bea5cab436b94e
chos946 <chosthea96@gmail.com>
2025-02-20 09:23:51 +0700

    Update group-off.js

--- plugins/group/group-off.js
+++ plugins/group/group-off.js
@@ -1,12 +1,12 @@
 exports.default = {
    names: ['Group Menu'],
-   tags: ['groupoff', 'tutup'],
-   command: ['groupoff', 'tutup'],
+   tags: ['groupoff', 'tutup','gc close'],
+   command: ['groupoff', 'tutup','gc close'],
    start: async (m, {
       conn
    }) => {
       await conn.groupSettingUpdate(m.chat, "announcement");
-      m.reply(`Group Telah Di Tutup Semua Anggota Tidak Dapat Mengirim Pesan Admin Kontol`)
+      m.reply(`Group Telah Di Tutup Semua Anggota Tidak Dapat Mengirim Pesan Admin`)
    },
    group: true,
    admin: true
